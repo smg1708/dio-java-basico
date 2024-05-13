cce685fe85e41e84ec4c2058773a43ff2605bcd6
ned <develid.io@gmail.com>
2024-05-13 11:13:50 -0300

    update aula 3

---
+++ dev-Santander/src/Aula3/Aula3.java
@@ -0,0 +1,22 @@
+package Aula3;
+
+public class Aula3 {
+    public static void main(String[] args) {
+        //tipos primitivos
+        //estudem a classe String que representa texto na aplicação
+        String meuNome = "Samuel Sousa";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+    }
+}
