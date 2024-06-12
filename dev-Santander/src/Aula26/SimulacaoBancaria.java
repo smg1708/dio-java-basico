3d4a5763f6ca1a2755ea6bd9f29c7f32688e0b58
ned <develid.io@gmail.com>
2024-06-12 10:33:28 -0300

    update

---
+++ dev-Santander/src/Aula26/SimulacaoBancaria.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        boolean continuar = true;
+
+        while (continuar) {
+            
+            int opcao = scanner.nextInt();
+
+            switch (opcao) {
+                case 1:
+                    double deposito = scanner.nextDouble();
+                    saldo += deposito;
+                    System.out.printf("Saldo atual: %.1f\n", saldo);
+                    
+                    break;
+
+                case 2:
+                    double saque = scanner.nextDouble();
+                    if (saldo >= saque) {
+                        saldo -= saque;
+                        System.out.printf("Saldo atual: %.1f\n", saldo);
+                    }
+                    else {
+                        System.out.println("Saldo insuficiente.");
+                    }
+
+                    break;
+
+                case 3:
+                    System.out.printf("Saldo atual: %.1f\n", saldo);
+                    
+                    break;
+
+                case 0:
+                    System.out.println("Programa encerrado.");
+                    continuar = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
+        scanner.close();
+    }
+}
